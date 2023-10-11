c9cc2b1bf637f9b0c791807910fbf4bfdb5cf2ee
kelvinadugyamfi <kelvinadugyamfi00@gmail.com>
2023-10-11 21:29:22 +0000

    update: made the site bg images dynamic

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -1,12 +1,12 @@
 function Header(){
     return (
-        <header class="masthead">
-        <div class="container position-relative px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <div class="site-heading">
+        <header className="masthead" style={{backgroundImage: "url('assets/img/home-bg.jpg')"}}>
+        <div className="container position-relative px-4 px-lg-5">
+            <div className="row gx-4 gx-lg-5 justify-content-center">
+                <div className="col-md-10 col-lg-8 col-xl-7">
+                    <div className="site-heading">
                         <h1>Clean Blog</h1>
-                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        <span className="subheading">A Blog Theme by Start Bootstrap</span>
                     </div>
                 </div>
             </div>
